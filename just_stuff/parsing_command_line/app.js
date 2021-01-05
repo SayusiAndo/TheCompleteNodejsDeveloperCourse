e33adb64484f7425991d82cfa812c3b2466f8472
András Csányi <sayusi.ando@gmail.com>
2021-01-05 22:00:19 +0100

    yargs less sucks
    Signed-off-by: András Csányi <sayusi.ando@gmail.com>

--- just_stuff/parsing_command_line/app.js
+++ just_stuff/parsing_command_line/app.js
@@ -1,41 +1,44 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
+const yargs = require('yargs/yargs')(process.argv.slice(2));
 
-// Customize yargs version
-yargs.version('1.1.0')
-
-// Create add command
-yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!')
-    }
-})
-
-// Create remove command
-yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing the note')
-    }
-})
-
-// Create list command
-yargs.command({
-    command: 'list',
-    describe: 'List your notes',
-    handler: function () {
-        console.log('Listing out all notes')
-    }
-})
-
-// Create read command
-yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    handler: function () {
-        console.log('Reading a note')
-    }
-})
+// noinspection BadExpressionStatementJS
+yargs
+    .command({
+        command: 'add',
+        aliases: ['-a', '--add'],
+        desc: 'Adds a new note',
+        builder: (yargs) => yargs.default('value', 'true'),
+        handler: (argv) => {
+            console.log('Adds function is executed')
+        }
+    })
+    .command({
+        command: 'remove',
+        aliases: ['-r', '--remove'],
+        desc: 'removes an item',
+        builder: (yargs) => yargs.default('value', 'true'),
+        handler: (argv) => {
+            console.log('Removing and item function is executed')
+        }
+    })
+    .command({
+        command: 'list',
+        aliases: ['-l', '--list'],
+        desc: 'Lists notes',
+        builder: (yargs) => yargs.default('value', 'true'),
+        handler: (argv) => {
+            console.log('List function is executed')
+        }
+    })
+    .command({
+        command: 'read',
+        aliases: ['-r', '--read'],
+        desc: 'Reads and item',
+        builder: (yargs) => yargs.default('value', 'true'),
+        handler: (argv) => {
+            console.log('Read an item function is executed')
+        }
+    })
+    .demandCommand()
+    .help()
+    .wrap(72)
+    .argv;
